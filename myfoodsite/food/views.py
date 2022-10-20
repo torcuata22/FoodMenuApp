c5dfe7636bde941c832a7c2294d721a70875d961
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-20 11:29:25 -0500

    detail view

--- myfoodsite/food/views.py
+++ myfoodsite/food/views.py
@@ -1,3 +1,4 @@
+import http
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
@@ -15,3 +16,11 @@ def index(request):
 
 def item(request):
      return HttpResponse('<h1>This is the item view</h1>')
+
+def detail (request, item_id):
+     item = Item.objects.get(pk = item_id)
+     context = {
+          'item':item,
+     }
+
+     return render(request, 'food/detail.html', context)
