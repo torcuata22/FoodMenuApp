ae6204ecc755d65fc8027492f1b50b08a65b1cb8
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-20 12:32:40 -0500

    statif

--- myfoodsite/food/urls.py
+++ myfoodsite/food/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+app_name = 'food'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name = 'item',),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
