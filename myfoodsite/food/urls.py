72de7847d7e25f455d26f5b951bb5f1036c2d553
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-20 16:08:16 -0500

    lecture 42, delete functionaliy not working

--- myfoodsite/food/urls.py
+++ myfoodsite/food/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('item/', views.item, name = 'item',),
     path('add/', views.create_item, name='create_item'),
     path('update/<int:id>',views.update_item, name='update_item'),
+    path('delete/<int:id>', views.delete_item, name='delete_item'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
