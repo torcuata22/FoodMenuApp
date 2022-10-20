d2faa290490586f6a339eb5ed7bd6574d8203d68
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-20 13:44:48 -0500

    added db field, migrations

--- myfoodsite/food/models.py
+++ myfoodsite/food/models.py
@@ -5,6 +5,8 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://pluspack.com/wp-content/uploads/2018/09/placeholder-picture.jpg")
+
 
     def __str__(self):
         return self.item_name
