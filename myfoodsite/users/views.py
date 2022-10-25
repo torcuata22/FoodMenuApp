eb0d1763392556019e288de605a6268cc985ec52
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-24 20:44:33 -0500

    messages

--- myfoodsite/users/views.py
+++ myfoodsite/users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def register (request):
-    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, your account has been created')
+            return redirect ('food:index')
+    else:
+        form = UserCreationForm
     return render(request, 'users/register.html', {'form': form})
