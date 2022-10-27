b1e38e0be186133f2f092027fc3e84d1f465a6d2
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-27 14:04:55 -0500

    restricted rountes

--- myfoodsite/myfoodsite/settings.py
+++ myfoodsite/myfoodsite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'food:index'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
