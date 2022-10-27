040a8dc9fc47ab4c4dbf4af64286eba9112b4c6d
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-27 14:18:58 -0500

    created profile model

--- myfoodsite/users/models.py
+++ myfoodsite/users/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE) #associates User and Profile, now we can add fields:
+    image = models.ImageField(default='profilepic.jpg', upload_to='profile_pictures')
+    location=models.CharField(max_length=100)
