7a52b1424ff52745bc1165e3547e47fe90da819a
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-10-28 13:33:01 -0500

    database relations are FUCKED, must redo at some point, ALL OF IT

--- myfoodsite/food/models.py
+++ myfoodsite/food/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User #Django's built-in User model
 # Create your models here.
 class Item(models.Model):
+    #user_name = models.ForeignKey(User, on_delete = models.CASCADE) MUST REDO THIS
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
